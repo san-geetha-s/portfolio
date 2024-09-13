7398ef6f3b7c2a1cd3a243a5a4a4fbb6bca0787f
san-geetha-s <101059528+san-geetha-s@users.noreply.github.com>
2024-09-13 13:35:45 +0530

    Add files via upload
    portfolio website

---
+++ src/App.js
@@ -0,0 +1,30 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="left_margin">
+    
+
+      </div>
+     <div className="right_margin">
+  
+     <div className="left_part">
+
+     </div>
+      <div className="right_part">
+
+     </div>
+    
+    </div>
+      
+     
+        
+       
+     
+    </div>
+  );
+}
+
+export default App;
